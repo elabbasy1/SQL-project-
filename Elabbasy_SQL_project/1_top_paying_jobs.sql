2d747723da52293180c79f1d7a3b73609305700a
Mohamed Elabbasy <mohamedelabbasy888@gmail.com>
2025-07-16 01:12:04 +0100

    Add Basic folder and updates

--- Elabbasy_SQL_project/1_top_paying_jobs.sql
+++ Elabbasy_SQL_project/1_top_paying_jobs.sql
@@ -20,10 +20,12 @@ FROM
 
 LEFT JOIN company_dim  ON job_postings_fact.company_id = company_dim.company_id
 WHERE
-  job_title_short = 'Data Scientist' AND 
+  job_title_short IN  ('Data Scientist', 'Data Analyst') AND 
   job_location = 'Ireland'  AND salary_year_avg IS NOT NULL
 
 ORDER BY
   salary_year_avg DESC
 
 LIMIT 10;
+
+
--- Elabbasy_SQL_project/3_top_demanded_skills.sql
+++ Elabbasy_SQL_project/3_top_demanded_skills.sql
@@ -1,14 +1,23 @@
 /*
-Question: Which skills are most in demand for Data Analyst roles?
+Question: Which skills are most in demand for Data Analyst and Data Scientist roles?
 - Perform an inner join on job postings as done in Query 2
-- Extract the top 5 most frequently requested skills for data analysts
+- Extract the top 5 most frequently requested skills for data analysts and scientists
 - Consider all available job postings
 - Purpose: To highlight the 5 most sought-after skills in the market, 
   offering valuable guidance for job seekers aiming to enhance their skillset.
 */
 
-SELECT *
+SELECT 
+ skills,
+ COUNT(skills_job_dim.skill_id) AS demand_count
 FROM job_postings_fact
 INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
 INNER JOIN skills_dim ON skills_job_dim.skill_id= skills_dim.skill_id
+WHERE
+  job_title_short IN ('Data scientist', 'Data Analyst') AND 
+  job_work_from_home = True
+GROUP BY
+  skills
+ORDER BY
+  demand_count DESC
 LIMIT 5;
